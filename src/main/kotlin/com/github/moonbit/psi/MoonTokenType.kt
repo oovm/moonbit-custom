0b435c97810ca31a91fe1ba45b5f7699e5f2031e
Aster <aster@vers.site>
2024-02-29 07:38:00 +0000

    feat: relink file extensions

---
+++ src/main/kotlin/com/github/moonbit/psi/MoonTokenType.kt
@@ -0,0 +1,8 @@
+package com.github.moonbit.psi
+
+import com.github.bytecodealliance.MoonLanguage
+import com.intellij.psi.tree.IElementType
+
+class MoonTokenType(debugName: String) : IElementType(debugName, MoonLanguage) {
+
+}
