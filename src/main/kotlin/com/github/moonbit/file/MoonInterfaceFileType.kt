3f2889710007580845d4feda186dd7d86d0211e2
Aster <galaster@foxmail.com>
2024-03-27 22:40:49 +0000

    fix: language bind file types

--- src/main/kotlin/com/github/moonbit/file/MoonInterfaceFileType.kt
+++ src/main/kotlin/com/github/moonbit/file/MoonInterfaceFileType.kt
@@ -1,26 +1,26 @@
 package com.github.moonbit.file
 
+import com.github.moonbit.MbtiLanguage
 import com.github.moonbit.MessageBundle
 import com.github.moonbit.MoonLanguage
 import com.intellij.openapi.fileTypes.LanguageFileType
 import javax.swing.Icon
 
-class MoonFileType : LanguageFileType {
+class MoonInterfaceFileType : LanguageFileType {
     /// use `INSTANCE`
-    private constructor() : super(MoonLanguage)
+    private constructor() : super(MbtiLanguage)
 
-    override fun getName(): String = MoonLanguage.id
+    override fun getName(): String = MbtiLanguage.id
 
     override fun getDescription(): String = MessageBundle.message("filetype.x.description")
 
-    override fun getDefaultExtension(): String = "mbt;mbti;"
+    override fun getDefaultExtension(): String = "mbti;"
 
     override fun getIcon(): Icon = MoonIconProvider.Moon
 
     @Suppress("CompanionObjectInExtension")
     companion object {
         @JvmStatic
-        val INSTANCE = MoonFileType()
+        val INSTANCE = MoonInterfaceFileType()
     }
 }
-
