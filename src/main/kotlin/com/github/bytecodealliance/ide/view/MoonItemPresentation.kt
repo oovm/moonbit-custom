9f6dc36bba359f1150b7026621e5fdd01ac1caaf
Aster <aster@vers.site>
2024-06-24 16:26:52 +0800

    define the mixin trait

--- src/main/kotlin/com/github/bytecodealliance/ide/view/MoonItemPresentation.kt
+++ src/main/kotlin/com/github/bytecodealliance/ide/view/MoonItemPresentation.kt
@@ -3,8 +3,17 @@ package com.github.bytecodealliance.ide.view
 import com.intellij.navigation.ItemPresentation
 import javax.swing.Icon
 
-class WitItemPresentation(private val icon: Icon, val text: String, private val detail: String? = null) :
-    ItemPresentation {
+class MoonItemPresentation : ItemPresentation {
+    private val icon: Icon
+    val text: String
+    private val detail: String?
+
+    constructor(icon: Icon, text: String, detail: String? = null) {
+        this.icon = icon
+        this.text = text
+        this.detail = detail
+    }
+
     override fun getPresentableText(): String = text
     override fun getIcon(unused: Boolean): Icon = icon
     override fun getLocationString(): String? = detail
